0143b222f275a79bb400d4631f822c51a13baebd
SharifMJahid <S398772@students.cdu.edu.au>
2026-01-11 14:33:44 +0930

    Add required imports for turtle and drawing modules

---
+++ Q3/Geometric Pattern-Python Turtle Graphic/main.py
@@ -0,0 +1,3 @@
+import turtle
+import recursive_edge  # this has our drawing function and keeps track of min/max positions
+from polygon import draw_polygon  # this draws the whole polygon using recursive edges
