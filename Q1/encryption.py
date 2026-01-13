4a5aed0540e1cfd075281fbdc5fc547c57646c66
gaurav-s397446 <s397446@students.cdu.edu.au>
2026-01-14 02:35:02 +0930

    encryption logic for uppercase

--- Q1/encryption.py
+++ Q1/encryption.py
@@ -1,5 +1,5 @@
 def encrypt(char, shift1, shift2):
-    
+    if char.islower():
         if 'a' <= char <= 'm':
             offset = ord(char) - ord('a')
             shiftVal = (shift1 * shift2) % 13
@@ -10,4 +10,14 @@ def encrypt(char, shift1, shift2):
             shiftVal = (shift1 + shift2) % 13
             position = (offset - shiftVal) % 13
             return chr(position + ord('n'))
-     
+    elif char.isupper():
+        if 'A' <= char <= 'M':
+            offset = ord(char) - ord('A')
+            shiftVal = shift1 % 13
+            position = (offset - shiftVal) % 13
+            return chr(position + ord('A'))
+        else:
+            offset = ord(char) - ord('N')
+            shiftVal = (shift2 * shift2) % 13
+            position = (offset + shiftVal) % 13
+            return chr(position + ord('N'))
