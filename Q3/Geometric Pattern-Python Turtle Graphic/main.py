adf3d61aa6cf0e70d00625fe1bc450abe3bbe1e9
SharifMJahid <S398772@students.cdu.edu.au>
2026-01-14 08:59:55 +0930

    Add first invisible drawing pass to measure polygon size

--- Q3/Geometric Pattern-Python Turtle Graphic/main.py
+++ Q3/Geometric Pattern-Python Turtle Graphic/main.py
@@ -10,3 +10,13 @@ def main():
     turtle.speed()  # make the turtle draw fast
     turtle.shape("turtle")  # show the turtle icon
     turtle.tracer(0, 0)  # stop animation for first invisible pass (faster) 
+    
+     # ---------- FIRST PASS (measure size, invisible) ----------
+    turtle.hideturtle()  # hide the turtle so we don't see first pass
+    turtle.penup()  # lift pen so we don't draw yet
+    turtle.goto(0, 0)  # start at the center
+    turtle.setheading(0)  # face right
+    turtle.pendown()  # put pen down to start drawing
+
+    draw_polygon(sides, length, depth)  # draw once invisibly to measure bounds
+
