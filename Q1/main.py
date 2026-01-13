01d6bfe130d776a65914c97fd71d9112777a27d8
gaurav-s397446 <s397446@students.cdu.edu.au>
2026-01-14 02:45:23 +0930

    added exception handling logic

--- Q1/main.py
+++ Q1/main.py
@@ -3,11 +3,16 @@ from decryption import decryptFile
 from verification import verify 
 
 def main():
-    shift1 = int(input("Enter shift1 value:"))
-    shift2 = int(input("Enter shift2 value:"))
-    encryptFile("raw_text", shift1, shift2)
-    decryptFile(shift1, shift2)
-    print(verify())
+    while True:
+        try:
+            shift1 = int(input("Enter shift1 value:"))
+            shift2 = int(input("Enter shift2 value:"))
+            encryptFile("raw_text", shift1, shift2)
+            decryptFile(shift1, shift2)
+            print(verify())
+            break
+        except ValueError:
+            print("Invalid input! Please enter a valid integer.")
 
 if __name__ == "__main__":
     main()
