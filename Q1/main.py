9403ec16791a55edd33232ec4825700dc3d7db29
gaurav-s397446 <s397446@students.cdu.edu.au>
2026-01-14 02:41:56 +0930

    adding logic to define main logic and execute it with references

--- Q1/main.py
+++ Q1/main.py
@@ -1,2 +1,13 @@
-shift1 = int(input("Enter shift1 value:"))
-shift2 = int(input("Enter shift2 value:"))
+from encryption import encryptFile 
+from decryption import decryptFile 
+from verification import verify 
+
+def main():
+    shift1 = int(input("Enter shift1 value:"))
+    shift2 = int(input("Enter shift2 value:"))
+    encryptFile("raw_text", shift1, shift2)
+    decryptFile(shift1, shift2)
+    print(verify())
+
+if __name__ == "__main__":
+    main()
