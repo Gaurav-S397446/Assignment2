93958c621c372758f0af2ca46886a53414697793
gaurav-s397446 <s397446@students.cdu.edu.au>
2026-01-14 06:09:46 +0930

    documentation of verification file

--- Q1/verification.py
+++ Q1/verification.py
@@ -1,9 +1,15 @@
 def verify():
+    """
+    This function verify if original text and decrypted file text matches or not.
+
+    Returns:
+        str: Success or Failure message
+    """
     with open("raw_text.txt","r") as rawFile:
-        rawTxt = rawFile.read()
+        rawTxt = rawFile.read() # reads the text of raw_file
     with open("decrypted_text.txt","r") as decryptedFile:
-        decryptedTxt = decryptedFile.read()
-    if(decryptedTxt == rawTxt):
+        decryptedTxt = decryptedFile.read() #reads the text of decrypted
+    if(decryptedTxt == rawTxt): # checking the content matches or not to find out encryption and decryption are performed correctly
         return "decryption successful!"
     else:   
         return "decryption failed!"
