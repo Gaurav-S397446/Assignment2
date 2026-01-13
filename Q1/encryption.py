1c683844a71302dbef4afe12e4349d6731c926da
gaurav-s397446 <s397446@students.cdu.edu.au>
2026-01-14 02:36:21 +0930

    added logic for reading file, encrypting text and re-writing in encryption file

--- Q1/encryption.py
+++ Q1/encryption.py
@@ -1,4 +1,6 @@
 def encrypt(char, shift1, shift2):
+    if not char.isalpha():
+        return char
     if char.islower():
         if 'a' <= char <= 'm':
             offset = ord(char) - ord('a')
@@ -21,3 +23,13 @@ def encrypt(char, shift1, shift2):
             shiftVal = (shift2 * shift2) % 13
             position = (offset + shiftVal) % 13
             return chr(position + ord('N'))
+        
+def encryptFile(filename, shift1, shift2):
+    with open(f"{filename}.txt","r") as file:
+        text = file.read()
+    result = ""
+    for char in text:
+        result += encrypt(char,shift1,shift2)
+    
+    with open("encrypted_text.txt","w") as writeFile:
+        writeFile.write(result)
